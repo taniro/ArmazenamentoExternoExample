d6dc1bf14d32062129dc2d1981920bd0e3c0f527
tanirocr@gmail.com <alakazamxdgithub12>
2017-10-03 19:43:03 -0300

    Initial commit

---
+++ app/src/main/java/tads/eaj/com/armazenamentoexternoexample/MainActivity.java
@@ -0,0 +1,241 @@
+package tads.eaj.com.armazenamentoexternoexample;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.prefs.Preferences;
+
+
+/*
+
+ SOURCE: http://stackoverflow.com/questions/8854359/exception-open-failed-eacces-permission-denied-on-android
+ ADAPTED by taniro.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    EditText inputText;
+    TextView response;
+    Button saveButton1, readButton1;
+    Button saveButton2, readButton2;
+
+    private String filename = "SampleFile.txt";
+    private String filepath = "MyFileStorage";
+    File myExternalFile;
+    String myData = "";
+
+    // Storage Permissions
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputText = (EditText) findViewById(R.id.myInputText);
+        response = (TextView) findViewById(R.id.response);
+
+
+        saveButton1 = (Button) findViewById(R.id.saveExternalStorage);
+        saveButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (checkStorage() == false){
+                    return;
+                }else{
+                    myExternalFile = new File(getExternalFilesDir(filepath), filename);
+                }
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(myExternalFile);
+                    fos.write(inputText.getText().toString().getBytes());
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                inputText.setText("");
+                response.setText("SampleFile.txt adicionado a pasta privada do armazenamento externo...");
+            }
+        });
+
+        readButton1 = (Button) findViewById(R.id.getExternalStorage);
+        readButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                myData = "";
+
+                if (checkStorage() == false){
+                    return;
+                }else{
+                    myExternalFile = new File(getExternalFilesDir(filepath), filename);
+                }
+
+                try {
+                    FileInputStream fis = new FileInputStream(myExternalFile);
+                    DataInputStream in = new DataInputStream(fis);
+                    BufferedReader br =
+                            new BufferedReader(new InputStreamReader(in));
+                    String strLine;
+                    while ((strLine = br.readLine()) != null) {
+                        myData = myData + strLine;
+                    }
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                inputText.setText(myData);
+                response.setText("SampleFile.txt lido da pasta privada do armazenamento externo...");
+            }
+        });
+
+        saveButton2 = (Button) findViewById(R.id.saveExternalStoragePublic);
+        saveButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (checkStorage() == false){
+                    return;
+                }else{
+                    PermissionUtils.validate(MainActivity.this, 0, PERMISSIONS_STORAGE);
+                    myExternalFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), filename);
+                }
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(myExternalFile);
+                    fos.write(inputText.getText().toString().getBytes());
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                inputText.setText("");
+                response.setText("SampleFile.txt salvo na pasta publica do armazenamento externo...");
+            }
+        });
+
+        readButton2 = (Button) findViewById(R.id.getExternalStoragePublic);
+        readButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                myData = "";
+
+                if (checkStorage() == false){
+                    return;
+                }else{
+                    PermissionUtils.validate(MainActivity.this, 0, PERMISSIONS_STORAGE);
+                    myExternalFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), filename);
+                }
+
+                try {
+                    FileInputStream fis = new FileInputStream(myExternalFile);
+                    DataInputStream in = new DataInputStream(fis);
+                    BufferedReader br =
+                            new BufferedReader(new InputStreamReader(in));
+                    String strLine;
+                    while ((strLine = br.readLine()) != null) {
+                        myData = myData + strLine;
+                    }
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                inputText.setText(myData);
+                response.setText("SampleFile.txt lido da pasta pública do armazenamento externo...");
+            }
+        });
+    }
+
+    public boolean checkStorage(){
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            return false;
+        }
+        return true;
+    }
+
+
+    private static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int result : grantResults) {
+            if (result == PackageManager.PERMISSION_DENIED) {
+                // Alguma permissão foi negada, agora é com você :-)
+                alertAndFinish();
+                return;
+            }
+        }
+        // Se chegou aqui está OK :-)
+    }
+
+    private void alertAndFinish() {
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.app_name).setMessage("Para utilizar todas as funções desse aplicativo, você precisa aceitar o acesso ao armanamento externo.");
+            // Add the buttons
+            builder.setNegativeButton("Fechar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //faz nada...
+                }
+            });
+            builder.setPositiveButton("Permitir", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }
+            });
+            android.support.v7.app.AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+}
